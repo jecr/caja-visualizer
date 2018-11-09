1d3469ab0d13d450adb57d3dde39600a940af9cc
Jorge Cortés <jorge.ecro@gmail.com>
2018-11-09 16:06:50 -0600

    Added visualization code, sample data, created component to build networks

--- src/Network.js
+++ src/Network.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import BarChart from './BarChart';
+import data from './mockdata/sampledata.json';
+import NetworkVis from './NetworkVis';
 
 const Network = (props) => (
   <div>
     You are now connected ~(ºoº)~
     The current network is: {props.match.params.networkID}
+    <NetworkVis
+      data={data}
+      width={500}
+      height={500}
+    />
     <BarChart
       data={[1,2,3,5,8,13,21,34]}
       width={700}
